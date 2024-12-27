26a77bf75dc0a581141292e9b450f7f702bc2a41
Quynh Anh <annabel.anh@gmail.com>
2024-12-27 14:21:55 -0700

    fix GitHub Pages server returns 404 after refreshing a page

--- react-components/vite.config.js
+++ react-components/vite.config.js
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/gfe-components/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        404: "404.html",
+      },
+    },
+  },
 });
