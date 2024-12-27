fc8df05c4fad3a54d0213670068433b3f029ab10
Quynh Anh <annabel.anh@gmail.com>
2024-12-26 18:08:55 -0700

    add HomePage

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import BadgesPage from "./pages/BadgesPage/BadgesPage.jsx";
 import ComponentPageLayout from "./layouts/ComponentPageLayout.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ComponentPageLayout />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="components" element={<ComponentPageLayout />}>
         <Route path="badge" element={<BadgesPage />} />
       </Route>
     </Routes>
---
+++ react-components/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router";
+
+function HomePage() {
+  const links = [{ to: "badge", label: "Badge" }];
+  return (
+    <>
+      <h1>Components</h1>
+      {links.map((link) => {
+        return (
+          <Link to={`components/${link.to}`} key={link.to}>
+            {link.label}
+          </Link>
+        );
+      })}
+    </>
+  );
+}
+
+export default HomePage;
