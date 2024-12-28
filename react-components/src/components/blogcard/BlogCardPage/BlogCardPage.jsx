5bf021b9be9f4e7d679a6863abf8d4c0e75a0501
Quynh Anh <annabel.anh@gmail.com>
2024-12-27 17:53:08 -0700

    jsx BlogCardPage

---
+++ react-components/src/components/blogcard/BlogCardPage/BlogCardPage.jsx
@@ -0,0 +1,31 @@
+import BlogCard from "../BlogCard";
+import coverImage from "../../../assets/spacejoy-YqFz7UMm8qE-unsplash.jpg";
+
+function BlogCardPage() {
+  const blogCard = {
+    image: {
+      url: coverImage,
+      alt: "Living Room",
+    },
+    badges: ["Interior"],
+    title: "Top 5 Living Room Inspirations",
+    desc: "Curated vibrant colors for your living, make it pop & calm in the same time.",
+    link: "example.com",
+  };
+
+  const { image, badges, title, desc, link } = blogCard;
+
+  return (
+    <div className="blog-card-page_container">
+      <BlogCard
+        image={image}
+        badges={badges}
+        title={title}
+        desc={desc}
+        link={link}
+      />
+    </div>
+  );
+}
+
+export default BlogCardPage;
